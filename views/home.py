70476e84cd3297b688176ece57fc6558968b7f6d
buetilia <buetilia@students.zhaw.ch>
2026-02-28 14:18:23 +0100

    updated name

--- views/home.py
+++ views/home.py
@@ -6,8 +6,8 @@ st.title("Meine erste Streamlit App")
 
 """
 Diese App wurde von folgenden Personen entwickelt:
-- Anna Müller (anmu@zhaw.ch)
-- Peter Meier (peme@zhaw.ch)
+- Lia Bütikofer (buetilia@students.zhaw.ch)
+- Vanessa Cakoncev (cakonvan@students.zhaw.ch)
 
 Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
 
